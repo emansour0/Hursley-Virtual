3c22e4d2e81bad9c03d309fba8f38256b470faf4
addybongo <adnan.ahmad@ntlworld.com>
2021-01-12 00:11:17 +0000

    Textures added and new first person controller

---
+++ Assets/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MouseSensitivity = 10;
+    private float xRotation = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity;
+
+        xRotation = Mathf.Clamp(xRotation - mouseY, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        transform.parent.transform.Rotate(Vector3.up * mouseX);
+    }
+}
